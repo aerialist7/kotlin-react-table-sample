aad74a2df235f451f7c699c1ef15fa9868d2cf16
aerialist7 <aerialist7@github.com>
2021-01-14 21:14:55 +0400

    Cleanup

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+import kotlinx.browser.document
+import react.child
+import react.dom.render
+
+fun main() {
+    render(document.getElementById("root")) {
+        child(UserTable) {
+            attrs {
+                headers = arrayOf(
+                    "Name", "Age"
+                )
+                users = arrayOf(
+                    User("Victor", 32),
+                    User("Alexander", 26),
+                    User("Catherine", 25),
+                    User("Nicolas", 25),
+                )
+            }
+        }
+    }
+}
