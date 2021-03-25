99c0be1d45db4bf4c9f60fde77776332396b96e3
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-03-25 17:19:16 +0100

    Switch to builtin definition

--- src/main/kotlin/Hooks.kt
+++
@@ -1,7 +0,0 @@
-import react.RCleanup
-import react.rawUseEffect
-
-fun useEffectWithCleanup(
-    vararg dependencies: dynamic,
-    callback: () -> RCleanup,
-) = rawUseEffect(callback, dependencies)
