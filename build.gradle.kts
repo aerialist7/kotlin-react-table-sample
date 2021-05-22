76e43098afe00f3afad86b2fbc2f78815fd392d3
aerialist7 <aerialist7@github.com>
2021-05-22 18:30:56 +0400

    Kotlin Wrappers 1.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,19 +4,18 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-js-wrappers")
 }
 
 fun kotlinw(target: String): String =
-    "org.jetbrains:kotlin-$target-pre.154-kotlin-1.4.32"
+    "org.jetbrains.kotlin-wrappers:kotlin-$target-pre.204-kotlin-1.5.0"
 
 dependencies {
     implementation(kotlinw("react:17.0.2"))
     implementation(kotlinw("react-dom:17.0.2"))
     implementation(kotlinw("react-table:7.7.0"))
-    implementation(kotlinw("styled:5.2.3"))
+    implementation(kotlinw("styled:5.3.0"))
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
 }
 
 kotlin.js {
