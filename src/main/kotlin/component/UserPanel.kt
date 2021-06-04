7a0e8275f31f4d27d75f2e029617f18feed94d38
aerialist7 <aerialist7@github.com>
2021-06-04 19:10:09 +0400

    Improve `UserPanel` component

--- src/main/kotlin/component/UserPanel.kt
+++ src/main/kotlin/component/UserPanel.kt
@@ -4,6 +4,7 @@ import Colors
 import hook.useSelectedUser
 import kotlinx.css.*
 import react.*
+import react.dom.div
 import styled.css
 import styled.styledDiv
 
@@ -11,33 +12,35 @@ external interface UserPanelProps : RProps
 
 private val UserPanel = functionalComponent<UserPanelProps> {
     val user = useSelectedUser()
-
-    styledDiv {
-        css {
-            width = 400.px
-            borderSpacing = 0.px
-            borderCollapse = BorderCollapse.collapse
-            whiteSpace = WhiteSpace.nowrap
-            borderWidth = 2.px
-            borderStyle = BorderStyle.solid
-            borderColor = Colors.Stroke.Gray
-            margin(LinearDimension.auto)
-        }
-
-        styledDiv {
-            +"Name: ${user?.name ?: "—"}"
+    div {
+        div {
+            +"Additional User Information"
         }
         styledDiv {
-            +"E-mail: ${user?.email ?: "—"}"
-        }
-        styledDiv {
-            +"Phone: ${user?.phone ?: "—"}"
-        }
-        styledDiv {
-            +"Website: ${user?.website ?: "—"}"
+            css {
+                width = 400.px
+                borderSpacing = 0.px
+                borderCollapse = BorderCollapse.collapse
+                whiteSpace = WhiteSpace.nowrap
+                borderWidth = 2.px
+                borderStyle = BorderStyle.solid
+                borderColor = Colors.Stroke.Gray
+                marginTop = 20.px
+            }
+            styledDiv {
+                +"Name: ${user?.name ?: "—"}"
+            }
+            styledDiv {
+                +"E-mail: ${user?.email ?: "—"}"
+            }
+            styledDiv {
+                +"Phone: ${user?.phone ?: "—"}"
+            }
+            styledDiv {
+                +"Website: ${user?.website ?: "—"}"
+            }
         }
     }
-
 }
 
 fun RBuilder.UserPanel(
