40c47f9505a669b8d1988bb752403e9fb63b7d93
aerialist7 <aerialist7@github.com>
2021-10-24 21:42:40 +0400

    Kotlin Wrappers v.0.0.1-pre.260-kotlin-1.5.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ fun kotlinw(target: String): String =
     "org.jetbrains.kotlin-wrappers:kotlin-$target"
 
 dependencies {
-    implementation(enforcedPlatform(kotlinw("wrappers-bom:0.0.1-pre.256-kotlin-1.5.31")))
+    implementation(enforcedPlatform(kotlinw("wrappers-bom:0.0.1-pre.260-kotlin-1.5.31")))
     implementation(kotlinw("react"))
     implementation(kotlinw("react-dom"))
     implementation(kotlinw("react-css"))
