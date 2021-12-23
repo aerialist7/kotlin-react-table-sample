5a23fddfc5017a593209b490b0d60d2735a07f13
Victor Turansky <victor.turansky@gmail.com>
2021-12-23 17:35:46 +0300

    Fix `Symbol` problem

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,13 @@ fun kotlinw(target: String): String =
     "org.jetbrains.kotlin-wrappers:kotlin-$target"
 
 dependencies {
-    implementation(enforcedPlatform(kotlinw("wrappers-bom:0.0.1-pre.282-kotlin-1.6.10")))
+    implementation(enforcedPlatform(kotlinw("wrappers-bom:0.0.1-pre.284-kotlin-1.6.10")))
     implementation(kotlinw("react"))
     implementation(kotlinw("react-dom"))
     implementation(kotlinw("react-css"))
     implementation(kotlinw("react-table"))
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
 }
 
 kotlin.js {
