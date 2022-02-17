16dec3d910b753459f7011ab6c33af3e1f03ddf0
aerialist7 <aerialist7@github.com>
2022-02-17 21:17:34 +0400

    Naming

--- src/main/kotlin/example/hook/useUsers.kt
+++ src/main/kotlin/example/hook/useUsers.kt
@@ -9,12 +9,12 @@ import kotlin.js.Promise
 fun useUsers(): Users {
     val result = useQuery<Users, Error, Users, String>(
         queryKey = QueryKey.USERS.name,
-        queryFn = { getUsers() }
+        queryFn = { readUsers() }
     )
     return result.data ?: emptyArray()
 }
 
-private fun getUsers(): Promise<Users> =
+private fun readUsers(): Promise<Users> =
     window.fetch("https://jsonplaceholder.typicode.com/users")
         .then { it.json() }
         .then { it.unsafeCast<Users>() }
