fed0428b3b07b2e2a752c34a0c5b262d18e60dd3
aerialist7 <aerialist7@github.com>
2022-02-19 18:36:07 +0400

    Fix `User` contract

--- src/main/kotlin/example/data/User.kt
+++ src/main/kotlin/example/data/User.kt
@@ -1,11 +1,11 @@
 package example.data
 
 external interface User {
-    val id: Key
-    val name: String
-    val email: String
-    val phone: String
-    val website: String
+    var id: Key
+    var name: String
+    var email: String
+    var phone: String
+    var website: String
 }
 
 typealias Users = Array<out User>
