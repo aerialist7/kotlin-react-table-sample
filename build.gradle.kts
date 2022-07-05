2d7602418dbdf3f7c5df660d1ef5ba5bd3631eb0
aerialist7 <aerialist7@github.com>
2022-07-05 16:28:25 +0400

    Wrappers `pre.351`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,12 @@
 plugins {
-    kotlin("js") version "1.6.21"
-}
-
-repositories {
-    mavenCentral()
+    kotlin("js")
 }
 
 fun kotlinw(target: String): String =
     "org.jetbrains.kotlin-wrappers:kotlin-$target"
 
 dependencies {
-    implementation(enforcedPlatform(kotlinw("wrappers-bom:1.0.0-pre.341")))
+    implementation(enforcedPlatform(kotlinw("wrappers-bom:1.0.0-pre.351")))
     implementation(kotlinw("react"))
     implementation(kotlinw("react-dom"))
     implementation(kotlinw("emotion"))
@@ -21,6 +17,11 @@ dependencies {
 kotlin.js {
     browser()
     binaries.executable()
+}
+
+// TODO: Remove when will be fixed in newest version of Kotlin
+extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
+    versions.webpackCli.version = "4.10.0"
 }
 
 tasks.wrapper {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,14 @@
 rootProject.name = "kotlin-react-table-sample"
+
+pluginManagement {
+    plugins {
+        val kotlinVersion = extra["kotlin.version"] as String
+        kotlin("js") version kotlinVersion
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
