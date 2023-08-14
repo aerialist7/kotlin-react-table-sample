012f431b6a8df4b3bcbbc2dae5a545b4158abb6e
Alexander Popoff <alexander.popoff@flysto.net>
2023-08-14 20:56:13 +0300

    Cleanup build config

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,10 @@ plugins {
 fun kotlinw(target: String): String =
     "org.jetbrains.kotlin-wrappers:kotlin-$target"
 
+val wrappersVersion = extra["kotlin.wrappers.version"]
+
 dependencies {
-    implementation(enforcedPlatform(kotlinw("wrappers-bom:1.0.0-pre.607")))
+    implementation(enforcedPlatform(kotlinw("wrappers-bom:$wrappersVersion")))
     implementation(kotlinw("react"))
     implementation(kotlinw("react-dom"))
     implementation(kotlinw("emotion"))
