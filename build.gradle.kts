95406ddc8ef90efc9787127d589a8c14fe92e4c3
Alexander Popoff <alexander.popoff@flysto.net>
2023-08-14 21:10:33 +0300

    Migrate from Kotlin `js` to `multiplatform` plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,30 @@
 plugins {
-    kotlin("js")
+    kotlin("multiplatform")
 }
 
 fun kotlinw(target: String): String =
     "org.jetbrains.kotlin-wrappers:kotlin-$target"
 
-val wrappersVersion = extra["kotlin.wrappers.version"]
+kotlin {
+    js {
+        browser()
+        binaries.executable()
+    }
 
-dependencies {
-    implementation(enforcedPlatform(kotlinw("wrappers-bom:$wrappersVersion")))
-    implementation(kotlinw("react"))
-    implementation(kotlinw("react-dom"))
-    implementation(kotlinw("emotion"))
-    implementation(kotlinw("tanstack-react-query"))
-    implementation(kotlinw("tanstack-react-table"))
-}
+    sourceSets {
+        val jsMain by getting {
+            val wrappersVersion = extra["kotlin.wrappers.version"]
 
-kotlin.js {
-    browser()
-    binaries.executable()
+            dependencies {
+                implementation(enforcedPlatform(kotlinw("wrappers-bom:$wrappersVersion")))
+                implementation(kotlinw("react"))
+                implementation(kotlinw("react-dom"))
+                implementation(kotlinw("emotion"))
+                implementation(kotlinw("tanstack-react-query"))
+                implementation(kotlinw("tanstack-react-table"))
+            }
+        }
+    }
 }
 
 tasks.wrapper {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "kotlin-react-table-sample"
 pluginManagement {
     plugins {
         val kotlinVersion = extra["kotlin.version"] as String
-        kotlin("js") version kotlinVersion
+        kotlin("multiplatform") version kotlinVersion
     }
 }
 
